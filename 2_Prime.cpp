8fa05bf7dbe84187f8daff5f9083bbdcfb23366b
Bishal Baira <bishalbaira432@gmail.com>
2024-05-18 19:27:34 +0530

    Feat: file added

---
+++ 2_Prime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for( i = 2; i<n;i++){
+        if(n%i==0){
+            cout<<"Not a prime number"<<endl;
+            break;
+        }
+    }
+    if(i==n){
+        cout<<n<<" is Prime number";
+    }
+}
