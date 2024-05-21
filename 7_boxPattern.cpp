5fe94f18f9b7763ba0a4e1d067426ffe9f3a7c21
Bishal Baira <bishalbaira432@gmail.com>
2024-05-22 01:44:16 +0530

    Feat: Files Add

---
+++ 7_boxPattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1,j=1;
+    while (i<=5)
+    {
+        j=1;
+        while (j<=5)
+        {
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+    
+}
