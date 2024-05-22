1f9f04c1664d63e2a53bc9bbe0191a56147e61b6
Bishal Baira <bishalbaira432@gmail.com>
2024-05-23 00:27:48 +0530

    Feat: File Add

---
+++ 8_pattern1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number" << endl;
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
